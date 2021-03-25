5a4a438595ec720ad0f71f5065859cf1147a5f43
Jim Wu <jimw567@gmail.com>
2021-03-25 08:25:59 -0700

    Added opencl-headers to install.sh

--- plugin/tigergraph/tests/cosine_nbor_ss_dense_int/match.sh
+++ plugin/tigergraph/tests/cosine_nbor_ss_dense_int/match.sh
@@ -34,13 +34,13 @@
 if [ "$load_cache" -eq 1 ]
 then
     echo "Caching cosine similarity vectors to patient vertices..."
-    gsql -g $xgraph "set query_timeout=240000000 run query cosinesim_cache_to_vertices()"
+    time gsql -g $xgraph "set query_timeout=240000000 run query cosinesim_cache_to_vertices()"
 fi
 
 if [ "$load_fpga" -eq 1 ]; then
     echo "Run query load_cu_cosinesim_ss_fpga and load_graph_cosinesim_ss_fpga"
-    gsql -g $xgraph "run query load_cu_cosinesim_ss_fpga($devices_needed)"
-    gsql -g $xgraph "run query load_graph_cosinesim_ss_fpga($devices_needed)"
+    time gsql -g $xgraph "run query load_cu_cosinesim_ss_fpga($devices_needed)"
+    time gsql -g $xgraph "run query load_graph_cosinesim_ss_fpga($devices_needed)"
 fi
 
 for ((j = 0 ; j < $iterations ; ++j))
@@ -49,9 +49,9 @@ do
 
     echo "------ iteration $j --------"
     echo "Run query cosinesim_ss_tg"
-    gsql -g $xgraph "run query cosinesim_ss_tg(\"$PWD/log/tg.txt\")"
+    time gsql -g $xgraph "run query cosinesim_ss_tg(\"$PWD/log/tg.txt\")"
     echo "Run query cosinesim_ss_fpga"
-    gsql -g $xgraph "run query cosinesim_ss_fpga(\"$PWD/log/fpga.txt\", $devices_needed)"
+    time gsql -g $xgraph "run query cosinesim_ss_fpga(\"$PWD/log/fpga.txt\", $devices_needed)"
     
     # basic checking of the result
     diff log/fpga.txt log/tg.txt
