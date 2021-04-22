1af694361f377b0b79e418facc3e543b4b06fc7b
Dave Liddell <dliddell@xilinx.com>
2021-04-22 14:03:05 -0700

    Deleted obsolete files

--- plugin/tigergraph/examples/synthea/old/newq.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo "Loading cache..."
-time gsql -g xgraph "set query_timeout=240000000 run query client_load_cache()"
-echo "Running SW cosine sim..."
-time gsql -g xgraph "set query_timeout=240000000 run query client_cosinesim_sw(10, \"37b6517f-844d-4201-99f1-f3e8911e2d2c\")"
