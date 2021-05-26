818d2b1c119710d426e10728018559b76b79f365
Kumar Deepak <kumar@xilinx.com>
2021-05-26 12:54:21 -0700

    Add support for max level

--- louvainmod/tests/create_partitions.sh
+++ louvainmod/tests/create_partitions.sh
@@ -7,22 +7,14 @@ script_dir=`dirname $script`
 
 echo Running $script
 if [ "$#" -lt 3 ]; then
-    echo "$script <.mtx file> <subdir name> <number of partitions>."
+    echo "$script <.mtx file> <partition project base name> <number of partitions>."
     echo "Example: $0 /proj/autoesl/ryanw/graph/Demo_For_webinary_WT/as-Skitter-wt.mtx skitter 9 -server_par $4"
     exit 1
 fi
 
 graph=$1
-subdir=$2
+projbasename=$2
 partitions=$3
-
-mkdir -p $subdir
-
-# Set rundir to your dir
-rundir=$subdir/louvain_partitions
-
-echo "Removing $rundir"
-rm -rf $rundir
 
 exe_dir="Release"
 if [ "$DEBUG" == "1" ]; then
@@ -30,7 +22,7 @@ if [ "$DEBUG" == "1" ]; then
 fi 
 
 export LD_LIBRARY_PATH=$script_dir/../$exe_dir/:$LD_LIBRARY_PATH
-cmd="$script_dir/../$exe_dir/louvainmod_test  $1 -fast -par_num $partitions -create_alveo_partitions -name $rundir -server_par $4"
+cmd="$script_dir/../$exe_dir/louvainmod_test  $1 -fast -par_num $partitions -create_alveo_partitions -name $projbasename -server_par $4"
 echo $cmd
 $cmd
 
--- louvainmod/tests/run_driver.sh
+++ louvainmod/tests/run_driver.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 echo Running $0
-if [ "$#" -lt 5 ]; then
-    echo "$0 <.mtx file> <partition-dir> <number of partitions> numDevices numWorkers"
+if [ "$#" -lt 6 ]; then
+    echo "$0 <.mtx file> <partition project base name> <number of partitions> <numDevices> <numWorkers> <max_level>"
     echo "Example: $0 /proj/gdba/datasets/louvain-graphs/as-Skitter-wt.mtx as-skitter-par9 9 3 2"
     exit 1
 fi
@@ -10,10 +10,11 @@ fi
 . env.sh
 
 graph=$1
-subdir=$2
+projdir=$2.par.proj
 par=$3
 num_dev=$4
 num_workers=$5
+numlevel=$6
 opt_out=
 if [ "$#" -eq 6 ]; then
     opt_out="-o $6"
@@ -22,8 +23,6 @@ fi
 workers="tcp://192.168.1.21:5555 tcp://192.168.1.31:5555"
 #workers="tcp://10.18.5.112:5555 tcp://10.18.5.113:5555"
 # Set rundir to your dir
-rundir=$subdir/louvain_partitions
-projdir=$rundir.par.proj
 xclbinfile=/proj/autoesl/ryanw/kernel_louvain_pruning.xclbin
 
 exe_dir="Release"
@@ -31,17 +30,7 @@ if [ "$DEBUG" == "1" ]; then
     exe_dir="Debug"
 fi 
 
-#make
-#./host.exe -x /proj/autoesl/ryanw/kernel_louvain_pruning.xclbin /proj/autoesl/ryanw/graph/Demo_For_webinary_WT/coPapersDBLP-wt.mtx  -fast  -dev 3 -par_num 12 -driver
-#./host.exe -x /proj/autoesl/ryanw/kernel_louvain_pruning.xclbin /proj/autoesl/ryanw/graph/Demo_For_webinary_WT/europe_osm-wt100M.mtx  -fast  -dev 3 -par_num 12 -driver
-#./host.exe -x /proj/autoesl/ryanw/kernel_louvain_pruning.xclbin /wrk/xsjhdnobkup1/ryanw/poc_louvain/HugeGraphData/europe_osm-wt600M.mtx  -fast  -dev 3 -par_num 12 -driver
-#./host.exe -x /proj/autoesl/ryanw/kernel_louvain_pruning.xclbin /wrk/xsjhdnobkup1/ryanw/poc_louvain/HugeGraphData/europe_osm-wt900M.mtx -fast -dev 3 -par_num 18 -driver
-#./host.exe -x /proj/autoesl/ryanw/kernel_louvain_pruning.xclbin /wrk/xsjhdnobkup1/ryanw/poc_louvain/HugeGraphData/europe_osm-wt400M_2.mtx -fast -dev 3 -par_num 9 -driver
-#./host.exe -x /proj/autoesl/ryanw/kernel_louvain_pruning.xclbin /wrk/xsjhdnobkup1/ryanw/poc_louvain/HugeGraphData/europe_osm-wt1350M.mtx -fast -dev 3 -par_num 27 -driver
-#./host.exe -x /proj/autoesl/ryanw/kernel_louvain_gh.xclbin /wrk/xsjhdnobkup1/ryanw/poc_louvain/HugeGraphData/europe_osm-wt600M.mtx  -dev 3 -par_num 12 -driver
-
-# e.g. ./run_driver.sh /proj/gdba/datasets/louvain-graphs/as-Skitter-wt.mtx as-skitter-par9 9
-cmd="../$exe_dir/louvainmod_test -x $xclbinfile $graph -fast -dev $num_dev -par_num $par \
+cmd="../$exe_dir/louvainmod_test -x $xclbinfile $graph -fast -dev $num_dev -num_level $numlevel -par_num $par \
           -load_alveo_partitions $projdir -setwkr $num_workers $workers -driverAlone $opt_out"
 echo $cmd
 $cmd
