56f0aaff02b9758f7a95000cf24f8fba0acebf7a
vivianw <vivianw@xilinx.com>
2021-06-15 13:47:57 -0700

    Moved the public fields as private and added setter and getter

--- plugin/tigergraph/comdetect/examples/comdetect/run.sh
+++ plugin/tigergraph/comdetect/examples/comdetect/run.sh
@@ -133,11 +133,11 @@ if [ "$run_mode" -eq 1 ] || [ "$run_mode" -eq 2 ]; then
     TOTAL_TIME=$(($(date +%s%3N) - START))
     echo "tg_partition_phase_3 " $TOTAL_TIME
     
-    START=$(date +%s%3N)
-    echo "Running louvain_alveo"
-    echo gsql -u $username -p $password -g $xgraph \'run query louvain_alveo\([\"Person\"], [\"Coworker\"], \"weight\",20,20,0.0001,FALSE,FALSE,\"\",\"/home2/tigergraph/output_alveo.txt\",TRUE,FALSE\)\'
-    time gsql -u $username -p $password -g $xgraph "run query louvain_alveo([\"Person\"], [\"Coworker\"], \
-         \"weight\",20,20,0.0001,FALSE,FALSE,\"\",\"/home2/tigergraph/output_alveo.txt\",TRUE,FALSE)"
-    TOTAL_TIME=$(($(date +%s%3N) - START))
-    echo "louvain_alveo: " $TOTAL_TIME
+    #START=$(date +%s%3N)
+    #echo "Running louvain_alveo"
+    #echo gsql -u $username -p $password -g $xgraph \'run query louvain_alveo\([\"Person\"], [\"Coworker\"], \"weight\",20,20,0.0001,FALSE,FALSE,\"\",\"/home2/tigergraph/output_alveo.txt\",TRUE,FALSE\)\'
+    #time gsql -u $username -p $password -g $xgraph "run query louvain_alveo([\"Person\"], [\"Coworker\"], \
+    #     \"weight\",20,20,0.0001,FALSE,FALSE,\"\",\"/home2/tigergraph/output_alveo.txt\",TRUE,FALSE)"
+    #TOTAL_TIME=$(($(date +%s%3N) - START))
+    #echo "louvain_alveo: " $TOTAL_TIME
 fi
