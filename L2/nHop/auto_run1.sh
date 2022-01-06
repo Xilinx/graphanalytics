19524c3ebb179b1ca8c3a496054c9b2dde103c56
Yunlei Zhang <yunleiz@xilinx.com>
2022-01-05 19:30:31 -0800

    [nhop] refine run.sh

--- L2/nHop/auto_run1.sh
+++ L2/nHop/auto_run1.sh
@@ -5,7 +5,8 @@ duplicate=0;
 testpath=/proj/xsjhdstaff5/yunleiz/nobkup/gitgragh/u50page/nhop/L2/tests/pocnHop
 datapath=/proj/xsjhdstaff5/siyangw/nobkup/git_hub_test/nhop10/L2/tests/nHop
 
-    build_dir.sw_emu.xilinx_u50_gen3x16_xdma_201920_3/host.exe -xclbin ./nHop_kernel.xclbin --offset $datapath/csr/europe_osm-csr-offset.mtx --index $datapath/csr//europe_osm-csr-indicesweights.mtx --pair $datapath/pair_small/europe_osm-twoHopPair.mtx --golden data/data-golden-small.mtx --batch $i --hop $hopNum --bypass $bypass --duplicate $duplicate --test
+    build_dir.sw_emu.xilinx_u50_gen3x16_xdma_201920_3/host.exe -xclbin $testpath/nHop_kernel.xclbin --offset $datapath/csr/europe_osm-csr-offset.mtx --index $datapath/csr//europe_osm-csr-indicesweights.mtx --pair $datapath/pair_small/europe_osm-twoHopPair.mtx --golden data/data-golden-small.mtx --batch $i --hop $hopNum --bypass $bypass --duplicate $duplicate --test
+    mkdir testResult
     mv summary.csv ./testResult/europe_osm-batch$i-hop$hopNum-bypass$bypass.csv
 
 #    build_dir.sw_emu.xilinx_u50_gen3x16_xdma_201920_3/host.exe -xclbin ./nHop_kernel.xclbin --offset $datapath/csr/cit-Patents-csr-offset.mtx --index $datapath/csr//cit-Patents-csr-indicesweights.mtx --pair $datapath/pair_small/cit-Patents-twoHopPair.mtx --golden data/data-golden-small.mtx --batch $i --hop $hopNum --bypass $bypass --duplicate $duplicate --test
